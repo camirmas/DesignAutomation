02c5a1b7095e023b9f013ec516c9b6f417db1926
Cameron Irmas <cirmas@protonmail.com>
2022-01-09 18:13:24 -0800

    Add acceleration step to CCS

--- src/ccs.jl
+++ src/ccs.jl
@@ -2,8 +2,8 @@ using LinearAlgebra
 using LineSearches, Optim
 
 """
-Implements the Cyclic Coordinate Search optimization method. Uses a Golden
-Section line search to determine step size.
+Implements the Cyclic Coordinate Search optimization method. Uses a Hager-Zhang
+linesearch to determine step size.
 """
 function ccs(fn, x0, ϵ=.001, max_iter=100)
     algo_hz = Newton(linesearch = HagerZhang())
@@ -18,14 +18,13 @@ function ccs(fn, x0, ϵ=.001, max_iter=100)
             d = zeros(n)
             d[i] = 1
 
+            # perform linesearch to determine step size
             α_fn(α) = fn(x + α .* d)
             res = Optim.optimize(α_fn, x, method=algo_hz)
 
             α = res.minimizer
             x += α .* d
-
             fx = fn(x)
-
             push!(results, (copy(x), fx))
 
             if norm(x-results[end-1][1]) < ϵ
@@ -33,6 +32,22 @@ function ccs(fn, x0, ϵ=.001, max_iter=100)
             end
 
             k += 1
+        end
+
+        # perform acceleration step to speed up optimization
+        d = x - results[k-n][1]
+        α_fn(α) = fn(x + α .* d)
+        res = Optim.optimize(α_fn, x, method=algo_hz)
+
+        α = res.minimizer
+        x += α .* d
+        fx = fn(x)
+        push!(results, (copy(x), fx))
+
+        k += 1
+
+        if norm(x-results[end-1][1]) < ϵ
+            return Response(x, fx, k)
         end
     end
 end
--- test/test_ccs.jl
+++ test/test_ccs.jl
@@ -5,15 +5,15 @@ using DesignAutomation
     x0 = [0., 0.]
     res = ccs(fn, x0)
     
-    @test res.minimizer == [5., 5.]
-    @test res.minimum == 0.
-    @test res.iterations == 2
+    @test res.minimizer == [5.000000000000287, 4.999999999999858]
+    @test res.minimum == 1.2269074772811875e-25
+    @test res.iterations == 4
 
     fn(x) = (2*x[2]-3*x[1]^2)^4 + (5-2*x[1])^2
     x0 = [0., 0.]
     res = ccs(fn, x0)
     
-    @test res.minimizer == [2.4999925903043896, 9.369964709290048]
-    @test res.minimum == 1.0058343546430942e-8
-    @test res.iterations == 46
+    @test res.minimizer == [2.5000000000414278, 9.37508062857279]
+    @test res.minimum == 6.761975408914261e-16
+    @test res.iterations == 2
 end
