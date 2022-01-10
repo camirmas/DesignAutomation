665dc9cb1275c2f071ca368a3d7f8d8324ee3417
Cameron Irmas <cirmas@protonmail.com>
2022-01-10 14:00:31 -0800

    Rework CCS + fix iteration count

--- src/ccs.jl
+++ src/ccs.jl
@@ -7,47 +7,36 @@ linesearch to determine step size.
 """
 function ccs(fn, x0, ϵ=.001, max_iter=100)
     algo_hz = Newton(linesearch = HagerZhang())
+    i = 1
     k = 0
     n = length(x0)
-    x = copy(x0)
     results = [(x0, fn(x0))]
+    e_hat = Matrix(1I, n, n)
 
-    for k = 1:max_iter
+    while k < max_iter
+        x, _ = results[end]
 
-        for i = 1:n
-            d = zeros(n)
-            d[i] = 1
-
-            # perform linesearch to determine step size
-            α_fn(α) = fn(x + α .* d)
-            res = Optim.optimize(α_fn, x, method=algo_hz)
-
-            α = res.minimizer
-            x += α .* d
-            fx = fn(x)
-            push!(results, (copy(x), fx))
-
-            if norm(x-results[end-1][1]) < ϵ
-                return Response(x, fx, k)
-            end
-
-            k += 1
+        if i == n + 1
+            d_k = x - results[k-n+1][1]
+            i = 1
+        else
+            d_k = e_hat[:, i]
         end
 
-        # perform acceleration step to speed up optimization
-        d = x - results[k-n][1]
-        α_fn(α) = fn(x + α .* d)
+        α_fn(α) = fn(x + α .* d_k)
         res = Optim.optimize(α_fn, x, method=algo_hz)
 
         α = res.minimizer
-        x += α .* d
-        fx = fn(x)
-        push!(results, (copy(x), fx))
+        x_new = x + α .* d_k
+        f_new = fn(x_new)
+        push!(results, (x_new, f_new))
 
         k += 1
+        i += 1
 
-        if norm(x-results[end-1][1]) < ϵ
-            return Response(x, fx, k)
+        if norm(x_new-x) < ϵ
+            return Response(x_new, f_new, k)
         end
+
     end
 end
--- test/test_ccs.jl
+++ test/test_ccs.jl
@@ -7,13 +7,13 @@ using DesignAutomation
     
     @test res.minimizer == [5.000000000000287, 4.999999999999858]
     @test res.minimum == 1.2269074772811875e-25
-    @test res.iterations == 4
+    @test res.iterations == 3
 
     fn(x) = (2*x[2]-3*x[1]^2)^4 + (5-2*x[1])^2
     x0 = [0., 0.]
     res = ccs(fn, x0)
     
-    @test res.minimizer == [2.5000000000414278, 9.37508062857279]
-    @test res.minimum == 6.761975408914261e-16
-    @test res.iterations == 2
+    @test res.minimizer == [2.4999999997617874, 9.374999962546214]
+    @test res.minimum == 2.2698089649468124e-19
+    @test res.iterations == 4
 end
